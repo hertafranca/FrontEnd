ce68f0996b0a8ead76b33cfb5d930f01f70f1a85
hertafranca <hertafranca.tech@gmail.com>
2020-01-18 02:46:13 -0200

    Stylization: profile, techs, username from github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+
+import './global.css';
 import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
 function App() {
+  useEffect(() => {  
+navigator.geolocation.getCurrentPosition(
+  (position) => {
+    console.log(position);
+  },
+  (err) => {
+    console.log(err);
+  },
+  {
+    timeout: 30000,
+  }
+)
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div id="app">
+     <aside>
+      <strong>Register</strong>
+      <form>
+        <div className='input-block'>
+          <label htmlFor="">The user from github</label>
+          <input name='github_username'id='github_username' required />
+        </div>
+
+        <div className='input-block'>
+          <label htmlFor="techs">Technologies</label>
+          <input name='Techs'id='Techs' required />
+        </div>
+
+        <div className="input-group">
+          <div className='input-block'>
+            <label htmlFor="latitude">latitude</label>
+            <input name='latitude'id='latitude' required />
+          </div>
+
+          <div className='input-block'>
+           <label htmlFor="longitude">longitude</label>
+            <input name='longitude'id='longitude' required />
+         </div>
+         </div>
+
+        <button type="submit">Save</button>
+      </form>
+     </aside>
+     <main>
+      <ul>
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars2.githubusercontent.com/u/59263959?s=460&v=4" alt="Herta Franca" />
+            <div className="user-info">
+              <strong>Herta Franca</strong>
+              <span>Html, Css, JavaScript</span>
+            </div>
+          </header>
+          <p>I'm new pathfinder of this technological world and an eternal learner of the web and mobile technologies.</p>
+          <a href="https://github.com/hertafranca">Access the profile in the github</a>
+        </li>
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars2.githubusercontent.com/u/59263959?s=460&v=4" alt="Herta Franca" />
+            <div className="user-info">
+              <strong>Herta Franca</strong>
+              <span>Html, Css, JavaScript</span>
+            </div>
+          </header>
+          <p>I'm new pathfinder of this technological world and an eternal learner of the web and mobile technologies.</p>
+          <a href="https://github.com/hertafranca">Access the profile in the github</a>
+        </li>
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars2.githubusercontent.com/u/59263959?s=460&v=4" alt="Herta Franca" />
+            <div className="user-info">
+              <strong>Herta Franca</strong>
+              <span>Html, Css, JavaScript</span>
+            </div>
+          </header>
+          <p>I'm new pathfinder of this technological world and an eternal learner of the web and mobile technologies.</p>
+          <a href="https://github.com/hertafranca">Access the profile in the github</a>
+        </li>
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars2.githubusercontent.com/u/59263959?s=460&v=4" alt="Herta Franca" />
+            <div className="user-info">
+              <strong>Herta Franca</strong>
+              <span>Html, Css, JavaScript</span>
+            </div>
+          </header>
+          <p>I'm new pathfinder of this technological world and an eternal learner of the web and mobile technologies.</p>
+          <a href="https://github.com/hertafranca">Access the profile in the github</a>
+        </li>
+      </ul>
+     </main>
+   </div>
   );
 }
 
