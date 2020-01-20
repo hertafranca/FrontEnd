e1db023420c9353e84901eca36e97fcc27b098be
hertafranca <hertafranca.tech@gmail.com>
2020-01-20 16:25:03 -0200

    Finalization of the FrontEnd Project

--- src/App.js
+++ src/App.js
@@ -1,101 +1,45 @@
-import React, { useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
+import api from './Services/api';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
 function App() {
-  useEffect(() => {  
-navigator.geolocation.getCurrentPosition(
-  (position) => {
-    console.log(position);
-  },
-  (err) => {
-    console.log(err);
-  },
-  {
-    timeout: 30000,
+  const [devs, setDevs] = useState([]);
+ 
+  useEffect(() => {
+   async function loadDevs() {
+    const response = await api.get('/devs');
+
+    setDevs(response.data);
+   } 
+   
+   loadDevs();
+  }, [])
+
+  
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
+
+    setDevs([...devs,response.data]);
   }
-)
-}, []);
 
   return (
    <div id="app">
      <aside>
       <strong>Register</strong>
-      <form>
-        <div className='input-block'>
-          <label htmlFor="">The user from github</label>
-          <input name='github_username'id='github_username' required />
-        </div>
-
-        <div className='input-block'>
-          <label htmlFor="techs">Technologies</label>
-          <input name='Techs'id='Techs' required />
-        </div>
-
-        <div className="input-group">
-          <div className='input-block'>
-            <label htmlFor="latitude">latitude</label>
-            <input name='latitude'id='latitude' required />
-          </div>
-
-          <div className='input-block'>
-           <label htmlFor="longitude">longitude</label>
-            <input name='longitude'id='longitude' required />
-         </div>
-         </div>
-
-        <button type="submit">Save</button>
-      </form>
+      <DevForm onSubmit={handleAddDev} />
      </aside>
+
      <main>
       <ul>
-        <li className="dev-item">
-          <header>
-            <img src="https://avatars2.githubusercontent.com/u/59263959?s=460&v=4" alt="Herta Franca" />
-            <div className="user-info">
-              <strong>Herta Franca</strong>
-              <span>Html, Css, JavaScript</span>
-            </div>
-          </header>
-          <p>I'm new pathfinder of this technological world and an eternal learner of the web and mobile technologies.</p>
-          <a href="https://github.com/hertafranca">Access the profile in the github</a>
-        </li>
-        <li className="dev-item">
-          <header>
-            <img src="https://avatars2.githubusercontent.com/u/59263959?s=460&v=4" alt="Herta Franca" />
-            <div className="user-info">
-              <strong>Herta Franca</strong>
-              <span>Html, Css, JavaScript</span>
-            </div>
-          </header>
-          <p>I'm new pathfinder of this technological world and an eternal learner of the web and mobile technologies.</p>
-          <a href="https://github.com/hertafranca">Access the profile in the github</a>
-        </li>
-        <li className="dev-item">
-          <header>
-            <img src="https://avatars2.githubusercontent.com/u/59263959?s=460&v=4" alt="Herta Franca" />
-            <div className="user-info">
-              <strong>Herta Franca</strong>
-              <span>Html, Css, JavaScript</span>
-            </div>
-          </header>
-          <p>I'm new pathfinder of this technological world and an eternal learner of the web and mobile technologies.</p>
-          <a href="https://github.com/hertafranca">Access the profile in the github</a>
-        </li>
-        <li className="dev-item">
-          <header>
-            <img src="https://avatars2.githubusercontent.com/u/59263959?s=460&v=4" alt="Herta Franca" />
-            <div className="user-info">
-              <strong>Herta Franca</strong>
-              <span>Html, Css, JavaScript</span>
-            </div>
-          </header>
-          <p>I'm new pathfinder of this technological world and an eternal learner of the web and mobile technologies.</p>
-          <a href="https://github.com/hertafranca">Access the profile in the github</a>
-        </li>
+        {devs.map(dev =>(
+          <DevItem key={dev._id} dev={dev} />
+        ))}
       </ul>
      </main>
    </div>
